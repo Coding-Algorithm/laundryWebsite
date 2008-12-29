1a1dadf52e63a74b525a62e014b55273a9fc738f
Ibrahim <taofeekibrahimope@gmail.com>
2008-12-29 02:18:14 -0800

    complete the mobile version

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 let hamburger = document.querySelector('.hamburger');
 let nav = document.querySelector('nav ul');
-let value = false;
+let value = true;
+
 
 openMenu = () => {
-    // console.log(value)
-    console.log(value)
+    console.log(value);
     open = value;
     console.log(open)
     if(open == true){
-        console.log('Yes');
         nav.style.visibility = 'visible';
         value = !value;
     }else if(open == false) {
