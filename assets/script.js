e0e489f2132c70efca94dd182ef635b7d044b1cb
Ibrahim <taofeekibrahimope@gmail.com>
2021-05-01 20:53:04 -0700

    Owk

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,10 @@ let hamburger = document.querySelector('.hamburger');
 let nav = document.querySelector('nav ul');
 let value = true;
 
+console.log('How are you>?'); //? 
 
 openMenu = () => {
-    console.log(value);
     open = value;
-    console.log(open)
     if(open == true){
         nav.style.visibility = 'visible';
         value = !value;
